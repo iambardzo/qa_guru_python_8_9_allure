853fa120e46749f48314c0484b3a6e26286cfb7b
chernenko_mi <chernenko_mi@magnit.ru>
2023-10-24 18:46:59 +0300

    hw_8_9

---
+++ tests/test_decorator.py
@@ -0,0 +1,46 @@
+import allure
+from selene.support import by
+from selene.support.shared import browser
+from selene.support.shared.jquery_style import s
+from allure_commons.types import Severity
+
+
+@allure.tag("web")
+@allure.severity(Severity.CRITICAL)
+@allure.label("owner", "eroshenkoam")
+@allure.feature("Задачи в репозитории")
+@allure.story("Проверка, что в репозитории есть Issue с номером 76")
+@allure.link("https://github.com", name="Testing")
+def test_decorator_steps():
+    open_main_page()
+    search_for_repository("eroshenkoam/allure-example")
+    go_to_repository("eroshenkoam/allure-example")
+    open_issue_tab()
+    should_see_issue_with_number("#76")
+
+
+@allure.step("Открываем главную страницу")
+def open_main_page():
+    browser.open("https://github.com")
+
+
+@allure.step("Ищем репозитория {repo}")
+def search_for_repository(repo):
+    s(".header-search-button").click()
+    s("#query-builder-test").send_keys("eroshenkoam/allure-example")
+    s("#query-builder-test").press_enter()
+
+
+@allure.step("Переходим по ссылке репозитория {repo}")
+def go_to_repository(repo):
+    s(by.link_text("eroshenkoam/allure-example")).click()
+
+
+@allure.step("Открываем таб Issues")
+def open_issue_tab():
+    s("#issues-tab").click()
+
+
+@allure.step("Проверяем наличие Issue с номером {number}")
+def should_see_issue_with_number(number):
+    s(by.partial_text(number)).click()
